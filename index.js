95ce31509820c57fd051fb2247a624a8725546f7
afkfish <makisbeni2003@gmail.com>
2022-02-26 22:22:04 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.get("/", (req, res, next) => {
+	return res.sendFile(path.join(__dirname + "/templates/main.html"));
+});
+
+app.get("/playlists", (req, res, next) => {
+	return res.send("<h1>Playlists");
+});
+
+app.get("/artists", (req, res, next) => {
+	return res.send("<h1>Artists");
+});
+
+app.listen(3000, () => {
+	console.log("Online on port 3000!");
+});
