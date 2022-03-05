7b0220ff33a6f6541039277255e5617fd722f470
afkfish <makisbeni2003@gmail.com>
2022-03-05 11:49:56 +0100

    newplaylist.html nad reformatting

--- index.js
+++ index.js
@@ -1,42 +1,45 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const session = require();
+const bodyParser = require("body-parser");
 
-app.use(express.static('static'));
+app.use(express.static("static"));
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-	return res.sendFile(__dirname + '/static/main.html');
+app.get("/", (req, res, next) => {
+	return res.sendFile(__dirname + "/static/main.html");
 });
 
 app.post(
-	'/',
+	"/",
 	(req, res, next) => {
-		if (typeof req.body.usr !== 'undefined') {
+		if (typeof req.body.usr !== "undefined") {
 			console.log(req.body.usr);
 			console.log(req.body.psswrd);
+			return next();
 		}
-		return next();
 	},
 	(req, res, next) => {
-		return res.redirect('/playlists');
+		return res.redirect("/playlists");
 	}
 );
 
-app.get('/playlists', (req, res, next) => {
-	return res.sendFile(__dirname + '/static/playlists.html');
+app.get("/playlists", (req, res, next) => {
+	return res.sendFile(__dirname + "/static/playlists.html");
 });
 
-app.get('/artists', (req, res, next) => {
-	return res.send('<h1/>Artists');
+app.get("/artists", (req, res, next) => {
+	return res.send("<h1/>Artists");
 });
 
-app.get('/registration', (req, res, next) => {
-	return res.send('<h1/>Registration');
+app.get("/registration", (req, res, next) => {
+	return res.send("<h1/>Registration");
+});
+
+app.get("/new_playlist", (req, res, next) => {
+	return res.sendFile(__dirname + "/static/newplaylist.html");
 });
 
 app.listen(3000, () => {
-	console.log('Online on port 3000!');
+	console.log("Online on port 3000!");
 });
